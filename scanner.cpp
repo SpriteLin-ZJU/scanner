0dbca700569930d410465679be8997b2c6ab8e54
SpriteLin_ZJU <317815056@qq.com>
2018-03-17 13:33:09 +0800

    更改字符集

---
+++ scanner.cpp
@@ -0,0 +1,12 @@
+#include "scanner.h"
+
+Scanner::Scanner()
+{
+	m_pLLT = new CInterfaceLLT("LLT.dll", &bLoadError);
+
+}
+
+Scanner::~Scanner()
+{
+	delete m_pLLT;
+}
--- scanner.h
+++ scanner.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "stdafx.h"
+#include <memory>
+
+#include "InterfaceLLT_2.h"
+
+
+class Scanner
+{
+public:
+	Scanner();
+	~Scanner();
+
+private:
+	static CInterfaceLLT* m_pLLT;
+	static unsigned int m_uiResolution;
+	static TScannerType m_tscanCONTROLType;
+
+	std::vector<unsigned int> m_vuiInterfaces;
+	std::vector<DWORD> vdwResolutions;
+	bool bLoadError;
+	int iRetValue;
+};
