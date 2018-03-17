5dbef4130f99b572a47c868d7aeba28abc9b98d7
SpriteLin_ZJU <317815056@qq.com>
2018-03-17 10:55:22 +0800

    添加scanner类

---
+++ stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <deque>
+#include <sstream>
+#include <fstream>
+#include <ostream>
+#include <istream>
+#include <iomanip>
+#include <algorithm>
